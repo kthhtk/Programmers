705415f0c6627aadf6e521aca9ba5a557c3fb31d
kthhtk <kth4061@gmail.com>
2024-11-07 19:45:40 +0900

    Create 11. a 와 b 출력하기.py

---
+++ Python/Lv 0/11. a 와 b 출력하기.py
@@ -0,0 +1,10 @@
+a, b = map(int, input().strip().split(' '))
+# print(a + b)
+
+while True :
+    if all(-100000 <= i <= 100000 for i in (a, b)) :
+        print(f'a = {a}')
+        print(f'b = {b}')
+        break
+    else :
+        continue
