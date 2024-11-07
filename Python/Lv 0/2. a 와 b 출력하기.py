fa5fbc1ec7d90e551e5d71bc0b372cb818972b29
kthhtk <kth4061@gmail.com>
2024-11-07 19:55:35 +0900

    Update 2. a 와 b 출력하기.py

--- Python/Lv 0/2. a 와 b 출력하기.py
+++ Python/Lv 0/2. a 와 b 출력하기.py
@@ -2,7 +2,7 @@ a, b = map(int, input().strip().split(' '))
 # print(a + b)
 
 while True :
-    if all(-100000 <= i <= 100000 for i in (a, b)) :
+    if all(-100000 <= i <= 100000 for i in (a, b)) : # while, break, else, continue 지워도 됨
         print(f'a = {a}')
         print(f'b = {b}')
         break
